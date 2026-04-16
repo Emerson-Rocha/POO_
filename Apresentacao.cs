54505410db06f2db543565490cb224b486a293c1
LabInfo <LabInfo@TERREOA2MPROF>
2026-04-16 10:56:42 -0300

    Adicionar arquivos de projeto.

---
+++ Apresentacao.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Net.Http.Headers;
+
+namespace POO
+{
+    class Apresentacao:Form
+    {
+        // Objeto
+     
+         Button btobj01  = new Button();
+ 
+        // toda classe tem um construtor
+        public Apresentacao() {
+            this.Text = "Titulo ";
+            this.Size = new Size(600, 600);
+            btobj01.Text = " Clicar ";
+            btobj01.Location = new Point(50, 50);
+            btobj01.Click += clicar;
+            this.Controls.Add(btobj01);
+        }
+
+        // metodo de Evento
+        private void clicar( object sender , EventArgs e ) {
+            MessageBox.Show("Metodo de Evento");
+        }
+
+
+    }
+}
